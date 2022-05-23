c889556b838f40ebe488ac559062af60358be887
Nathanael Setiawan <metalwhine@gmail.com>
2022-05-23 12:32:47 +0700

    IT WORKS

--- React/src/pages/CreatePost.jsx
+++ React/src/pages/CreatePost.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, {Component} from "react";
 
-const CreatePost = () => {  
-  return (
-    <div>
-      <form>
-        <input id='Name' type='text' name='name' placeholder="Name"></input>
-        <input id='Alias' type='text' name='alias' placeholder="Alias"></input>
-        <input id='Submit' type='submit' name="submit"></input>
-      </form>
-    </div>
-  );
+export class CreatePost extends Component{
+  constructor(props){
+    super(props);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    fetch('http://localhost:8000/heroes/',{
+        method:'POST',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            name:event.target.name.value,
+            alias:event.target.alias.value
+        })
+    })
+    .then(res=>res.json())
+    .then((result)=>{
+        alert(result);
+    },
+    (error)=>{
+        alert('Failed');
+    })
+}
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input id='Name' controlId='name' type='text' name='name' placeholder="Name"></input>
+          <input id='Alias' controlId='alias' type='text' name='alias' placeholder="Alias"></input>
+          <input id='Submit' type='submit' name="submit"></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreatePost;
--- React/src/pages/Home.jsx
+++ React/src/pages/Home.jsx
@@ -2,10 +2,45 @@ import React, {Component} from "react";
 
 class Home extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state={heroes:[]}
+  }
+
+  refreshList(){
+    fetch('http://localhost:8000/heroes/')
+    .then(response=>response.json())
+    .then(data=>{
+      this.setState({heroes:data});
+      console.log('Data: ', data);
+    });
+  }
+
+  componentDidMount(){
+    this.refreshList();
+  }
+
+  componentDidUpdate(){
+    this.refreshList();
+  }
+
   render(){
+    const {heroes}=this.state;
     return (
       <div>
-        Home
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Alias</th>
+          </tr>
+          {heroes.map(hero=>
+            <tr>
+              <td>{hero.name}</td>
+              <td>{hero.alias}</td>
+            </tr>
+            )}
+
+        </table>
       </div>
     );
   }
